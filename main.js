931381d88c0acd70609e3b87f4f6b8ffd46f3a11
João Victor Caires <srkayrez@gmail.com>
2022-09-18 19:52:44 -0300

    Create a table for exibition

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 let numeros = [
-    01, 02, 03, 04, 05, 06, 07, 08, 09, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-    51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-    61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-    71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-    81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
-    91, 92, 93, 94, 95, 96, 97, 98, 99
+    01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15,
+    16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+    46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75
 ];
 let numerosJaSorteados = [];
 let numeroSorteado;
@@ -19,6 +14,7 @@ function sortearNovoNumero(){
     numeros = numeros.filter(element => (element != numeroSorteado));
     ordenarElementos();
     alterarElementosHtml();
+    selecionarNaTabela();
 }
 
 function ordenarElementos(){
@@ -31,5 +27,9 @@ function ordenarElementos(){
 
 function alterarElementosHtml(){
     document.getElementById("numeroSorteado").innerHTML = numeroSorteado;
-    document.getElementById("numerosJaSorteados").innerHTML = numerosJaSorteados;
+}
+
+function selecionarNaTabela(){
+    document.getElementById(numeroSorteado).style.backgroundColor = 'rgb(96, 96, 185)';
+    document.getElementById(numeroSorteado).style.color = 'white';
 }
